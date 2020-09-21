c64769413833b5c336e06816eb8f898e0cc73005
Antonio Magalhães <antoniofmagalhaes@protonmail.com>
2020-09-21 18:03:09 -0400

    feat: google map component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,165 @@
 import 'dotenv/config';
 import React from 'react';
+import {
+  GoogleMap,
+  InfoWindow,
+  Marker,
+  useLoadScript,
+} from '@react-google-maps/api';
+import PropTypes from 'prop-types';
+import { formatRelative } from 'date-fns';
+import usePlacesAutocomplete, {
+  getGeocode,
+  getLatLng,
+} from 'use-places-autocomplete';
+import {
+  Combobox,
+  ComboboxInput,
+  ComboboxList,
+  ComboboxOption,
+  ComboboxPopover,
+} from '@reach/combobox';
+import libraries from './config/libraries';
 
 import GlobalStyles from './styles';
+import mapContainerStyle from './styles/mapContainerStyle';
+import mapStyle from './styles/mapStyle';
+import { SearchBox } from './styles/styles';
+
+const center = {
+  lat: -20.481984980707782,
+  lng: -54.61926970828739,
+};
+
+const options = {
+  styles: mapStyle,
+  disableDefaultUI: true,
+  zoomControl: true,
+};
 
 function App() {
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+    libraries,
+  });
+  const [markers, setMarkers] = React.useState([]);
+  const [selected, setSelected] = React.useState(null);
+
+  const onMapClick = React.useCallback(e => {
+    setMarkers(current => [
+      ...current,
+      {
+        lat: e.latLng.lat(),
+        lng: e.latLng.lng(),
+        time: new Date(),
+      },
+    ]);
+  }, []);
+
+  const mapRef = React.useRef();
+  const onMapLoad = React.useCallback(map => {
+    mapRef.current = map;
+  }, []);
+
+  const panTo = React.useCallback(({ lat, lng }) => {
+    mapRef.current.panTo({ lat, lng });
+    mapRef.current.setZoom(14);
+  }, []);
+
+  if (loadError) return 'error on loading maps';
+  if (!isLoaded) return 'loading';
+
   return (
     <>
-      <div />
+      <Search panTo={panTo} />
+
+      <GoogleMap
+        mapContainerStyle={mapContainerStyle}
+        zoom={13}
+        center={center}
+        options={options}
+        onClick={onMapClick}
+        onLoad={onMapLoad}
+      >
+        {markers.map(({ time, lat, lng }) => (
+          <Marker
+            key={time.toISOString()}
+            position={{ lat, lng }}
+            onClick={() => setSelected({ time, lat, lng })}
+          />
+        ))}
+        {selected && (
+          <InfoWindow
+            position={{ lat: selected.lat, lng: selected.lng }}
+            onCloseClick={() => setSelected(null)}
+          >
+            <div>
+              <p>seen {formatRelative(selected.time, new Date())}</p>
+              <p>
+                lat: {selected.lat} lng: {selected.lng}
+              </p>
+            </div>
+          </InfoWindow>
+        )}
+      </GoogleMap>
       <GlobalStyles />
     </>
   );
 }
+
+function Search({ panTo }) {
+  const {
+    ready,
+    value,
+    suggestions: { status, data },
+    setValue,
+    clearSuggestions,
+  } = usePlacesAutocomplete({
+    requestOptions: {
+      location: {
+        lat: () => center.lat,
+        lng: () => center.lng,
+      },
+      radius: 100 * 1000, // 100km
+    },
+  });
+
+  return (
+    <SearchBox>
+      <Combobox
+        onSelect={async address => {
+          setValue(address, false);
+          clearSuggestions();
+          try {
+            const results = await getGeocode({ address });
+            const { lat, lng } = await getLatLng(results[0]);
+            panTo({ lat, lng });
+          } catch (error) {
+            console.log(error);
+          }
+        }}
+      >
+        <ComboboxInput
+          value={value}
+          onChange={e => setValue(e.target.value)}
+          disabled={!ready}
+          placeholder="Search places..."
+        />
+        <ComboboxPopover>
+          <ComboboxList>
+            {status === 'OK' &&
+              data.map(({ place_id, description }) => (
+                <ComboboxOption key={place_id} value={description} />
+              ))}
+          </ComboboxList>
+        </ComboboxPopover>
+      </Combobox>
+    </SearchBox>
+  );
+}
+
+Search.propTypes = {
+  panTo: PropTypes.func.isRequired,
+};
 
 export default App;
